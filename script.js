14b9d1eb1d3f90e94ea9af35ee06395d33965543
Worker <sidney.rozas@gmail.com>
2025-02-01 21:55:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 // You can add any JavaScript functionality here
 console.log("Website Loaded!");
+
+ <script>
+        document.getElementById('navbar').innerHTML = fetch('nav.html')
+            .then(response => response.text())
+            .then(data => document.getElementById('navbar').innerHTML = data);
+    </script>
