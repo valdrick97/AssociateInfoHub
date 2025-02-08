12110a1c804a0c2a7ca1ea1ad91b6b67f2adc775
Worker <sidney.rozas@gmail.com>
2025-02-07 18:55:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ console.log("Website Loaded!");
     }
 });
 
+function toggleDropdown(dropdownId) {
+    var dropdown = document.getElementById(dropdownId);
+    dropdown.classList.toggle("show");
+}
+
+// Close dropdowns if user clicks outside
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            if (dropdowns[i].classList.contains('show')) {
+                dropdowns[i].classList.remove('show');
+            }
+        }
+    }
+}
